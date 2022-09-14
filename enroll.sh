9a77abf1cea33c5b3c6cb6a01c0a59e345a69bf6
Şahin Akkaya <sahin@sahinakkaya.dev>
2022-09-14 12:25:38 +0300

    Update registration url

--- enroll.sh
+++ enroll.sh
@@ -2,10 +2,10 @@
 
 url="https://kepler-beta.itu.edu.tr/api/TaslakKontrolAPI/v1/" # use this url while you are testing
 
-# url="https://kepler-beta.itu.edu.tr/api/ders-kayit/v2/"     # uncomment this line when you are ready
+# url="https://kepler-beta.itu.edu.tr/api/ders-kayit/v21/"     # uncomment this line when you are ready
 
 # get this token from course enrollment page as shown in the screenshot and paste it here
-token="Authorization: Bearer eyJhbGciO......" 
+token="Authorization: Bearer eyJhbGciO..." 
 
 # write the crn numbers of the lectures you want to take. crn's should be separated by space.
 crn_list=("20034" "20289") 
